68bc3606de908bb7092846ee8a27288dc34ab296
Patrick Lioi <patrick.lioi@gmail.com>
2013-09-06 15:23:53 -0500

    Complete the initial unit tests for iterations 00 and 01.

--- RefactoringPatternsResharperClassLevelDesign/src/Iteration01/Tests/TransactionImporterTests.cs
+++ RefactoringPatternsResharperClassLevelDesign/src/Iteration01/Tests/TransactionImporterTests.cs
@@ -21,16 +21,19 @@ namespace Iteration01.Tests
             transactions[0].Type.ShouldEqual(TransactionType.Deposit);
             transactions[0].Timestamp.ShouldEqual(new DateTime(2012, 2, 1, 13, 14, 25));
             transactions[0].Account.ShouldEqual("123456789");
+            transactions[0].Amount.ShouldEqual(150m);
             transactions[0].Description.ShouldEqual("Paycheck");
 
             transactions[1].Type.ShouldEqual(TransactionType.Deposit);
             transactions[1].Timestamp.ShouldEqual(new DateTime(2013, 3, 2, 9, 12, 32));
             transactions[1].Account.ShouldEqual("987654321");
+            transactions[1].Amount.ShouldEqual(200m);
             transactions[1].Description.ShouldEqual("Paycheck");
 
             transactions[2].Type.ShouldEqual(TransactionType.Withdrawal);
             transactions[2].Timestamp.ShouldEqual(new DateTime(2013, 3, 3, 8, 22, 33));
             transactions[2].Account.ShouldEqual("987654321");
+            transactions[2].Amount.ShouldEqual(123m);
             transactions[2].Description.ShouldEqual("Car Payment");
         }
     }
