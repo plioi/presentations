ba88e3527638e160024c0a5f971db05cf286eb80
Patrick Lioi <patrick.lioi@gmail.com>
2013-09-09 09:57:11 -0500

    Add "Code" metadata property to TransactionType.

--- RefactoringPatternsResharperClassLevelDesign/src/Iteration02/Model/TransactionType.cs
+++ RefactoringPatternsResharperClassLevelDesign/src/Iteration02/Model/TransactionType.cs
@@ -4,12 +4,13 @@ namespace Iteration02.Model
 {
     public class TransactionType : Enumeration<TransactionType>
     {
-        public static readonly TransactionType Deposit = new TransactionType(0, "Deposit");
-        public static readonly TransactionType Withdrawal = new TransactionType(1, "Withdrawal");
+        public static readonly TransactionType Deposit = new TransactionType(0, "Deposit", "D");
+        public static readonly TransactionType Withdrawal = new TransactionType(1, "Withdrawal", "W");
 
-        private TransactionType(int value, string displayName)
+        private TransactionType(int value, string displayName, string code)
             : base(value, displayName)
         {
+            Code = code;
         }
 
         public string Code { get; private set; }
